ce7e43dc81fbbe6b40d114e5bed0b19949d23faf
Naresh Madhur <nareshmadhur@gmail.com>
2025-07-20 20:03:12 +0000

    Lets keep a single font for the whole page - the content, the formatted

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
-        code: ['"Source Code Pro"', 'monospace'],
+        code: ['Inter', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
